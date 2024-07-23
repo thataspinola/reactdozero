d2f7cad680e3335f5a365f574dcb70fa01bf72e9
thataspinola <thata.arlekina@gmail.com>
2024-07-23 10:07:48 -0300

    curso

---
+++ 8_REACT_ROUTER/reactrouter/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+import { BrownserRouter, Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Router</h1>
+
+      <BrownserRouter>
+        <Routes>
+        </Routes>
+      </BrownserRouter>
+    </div>
+  );
+}
+
+export default App;

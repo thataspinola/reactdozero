c09245da7faf99e3bb07af0abbcf6df35222080f
Thamiris Spinola <thata.arlekina@gmail.com>
2024-07-30 20:24:41 -0300

    context

---
+++ 9_CONTEXT/context/src/pages/Products/Products.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Products = () => {
+  return (
+    <div>
+      <h1>Lista de produtos</h1>
+      <p>Valor contador: 0</p>
+    </div>
+  );
+};
+
+export default Products;
